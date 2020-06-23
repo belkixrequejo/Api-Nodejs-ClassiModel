1e39ec52407fb1608aefeccc7f91c7f897ba4909
belkixrequejo <belkix.requejo@pucese.edu.ec>
2020-06-23 08:22:44 -0500

    creacios del formulario

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ var dbConn  = require('./db');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+app.post('/ordenar', urlencodedParser, function (req, res) {
+    dbConn.query('CALL ordenar_producto('+req.body+')', function(err,rows) {
+            if(err) {
+               res.send(err)
+            } else {
+                res.send(rows)
+                res.send('Datos guardados exitosamente, ' + req.body.username)
+            }
+        })
+});
+
+
 
 app.get("/1/cliente",
     (req, res) => {
@@ -27,7 +47,10 @@ app.get("/1/producto",
             }
         })
     });
-
+   app.post('/users', (req, res) => {
+      users.push('User ' + users.length)
+      res.send("New user add")
+    });
 
 app.use((req, res) => {
     res.status(404).send({
